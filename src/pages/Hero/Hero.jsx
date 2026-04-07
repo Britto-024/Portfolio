163f8ff6f179a47d7a51a0d486784654a152b37f
Britto-024 <britto@example.com>
2026-04-07 10:07:32 +0530

    Update_to_3D_Particles

--- src/pages/Hero/Hero.jsx
+++ src/pages/Hero/Hero.jsx
@@ -1,9 +1,54 @@
+import React, { useRef, useMemo } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Points, PointMaterial } from '@react-three/drei';
 import "./Hero.css";
+
+function Particles({ count = 5000 }) {
+  const points = useRef();
+
+  const positions = useMemo(() => {
+    const pos = new Float32Array(count * 3);
+    for (let i = 0; i < count; i++) {
+        const r = 2 * Math.cbrt(Math.random());
+        const theta = Math.random() * 2 * Math.PI;
+        const phi = Math.acos(2 * Math.random() - 1);
+        pos[i * 3] = r * Math.sin(phi) * Math.cos(theta);
+        pos[i * 3 + 1] = r * Math.sin(phi) * Math.sin(theta);
+        pos[i * 3 + 2] = r * Math.cos(phi);
+    }
+    return pos;
+  }, [count]);
+
+  useFrame((state, delta) => {
+    if (points.current) {
+      points.current.rotation.x -= delta / 10;
+      points.current.rotation.y -= delta / 15;
+    }
+  });
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={points} positions={positions} stride={3} frustumCulled={false}>
+        <PointMaterial
+          transparent
+          color="#88ccff"
+          size={0.005}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
+}
 
 export default function Hero() {
   return (
     <div className="hero" id="hero">
-      <img src="/Photo.png" alt="Hero Background" className="hero-img" />
+      <div className="canvas-container">
+        <Canvas camera={{ position: [0, 0, 1] }}>
+          <Particles />
+        </Canvas>
+      </div>
       <div className="content">
         <div className="buttn">
           <a href="#contact" className="hero-btn">Contact</a>
@@ -11,6 +56,5 @@ export default function Hero() {
         </div>
       </div>
     </div>
-    
   );
 }
